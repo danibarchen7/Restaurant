99c7edcc8cecd4ccca547586a947dfc4abdc1138
danibrshien <danibarchene@gmail.com>
2025-04-02 21:22:05 +0300

    another edit

--- resturantA/resturantA/settings.py
+++ resturantA/resturantA/settings.py
@@ -85,13 +85,22 @@ WSGI_APPLICATION = 'resturantA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant_aziz',
+        'USER': 'dani',
+        'PASSWORD': os.getenv('DP_PASSWORD'),
+        'HOST': os.getenv('DP_HOST'),
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
