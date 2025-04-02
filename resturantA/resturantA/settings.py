864ea280ce702ef01f6badf4d797bfb194933a41
danibrshien <danibarchene@gmail.com>
2025-04-02 21:36:43 +0300

    another edit

--- resturantA/resturantA/settings.py
+++ resturantA/resturantA/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i-b7on3on^_%_@v&xgf5!gd)4hbuswdd=y!9o5kqk$pb30%opb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
