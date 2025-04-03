fde9ca7f19b651e9bb374cec9a431ff7db541e86
danibrshien <danibarchene@gmail.com>
2025-04-03 16:18:38 +0300

    try new editing on requirments file

--- resturantA/resturantA/settings.py
+++ resturantA/resturantA/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import dj_database_url
+# import dj_database_url
 # Load .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = BASE_DIR / '.env'
