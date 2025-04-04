92d62ef109505818700ef95a7c9d9b1ab28aca04
danibrshien <danibarchene@gmail.com>
2025-04-04 12:18:27 +0300

    modifing the build bash file

--- resturantA/build.sh
+++ resturantA/build.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Convert static asset files
-python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
+# Run migrations (ensure the database is ready)
 python manage.py migrate
+
+# Create superuser non-interactively
+echo "Creating superuser..."
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+User.objects.filter(username='$SUPERUSER_USERNAME').exists() or \
+User.objects.create_superuser('$SUPERUSER_USERNAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')" | python manage.py shell
