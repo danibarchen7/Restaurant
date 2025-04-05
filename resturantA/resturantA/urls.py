d67b58ee6fd6f5f82e3d3eaa494fe421b42ccdc3
danibrshien <danibarchene@gmail.com>
2025-04-05 20:22:54 +0300

    editing the urls file

--- resturantA/resturantA/urls.py
+++ resturantA/resturantA/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('Menu/', include('dishes.urls')),
     path('api/',include('api.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
